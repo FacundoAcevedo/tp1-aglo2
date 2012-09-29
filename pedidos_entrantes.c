43d23ef3cbf1e64e36e79d976ac76db12877a11e
Usuario <usuario@MacBook-Air-de-Usuario.local>
2012-09-29 16:18:25 -0300

    creado el struct pedidos_entrantes_t y arregladas las funciones que lo usan

--- pedidos_entrantes.c
+++ pedidos_entrantes.c
@@ -5,13 +5,19 @@
 #include "lista.h"
 
 typedef struct pedido pedido_t;
+
+typedef struct pedidos_entrantes pedidos_entrantes_t{
+	lista_t* lista_pedidos;
+	lista_iter_t* iter_lista_pedidos;
+} pedidos_entrantes_t;
+	
 struct pedido{
-	int zona; // es necesario el * ?
-	int cant_pizzas;  // es necesario el * ?
+	int zona;
+	int cant_pizzas;
 	unsigned int id;
 	} pedido_t;
 
-long get_id();
+unsigned int get_id();
 
 pedido_t* pedido_crear( int zona, int cant_pizzas){
 	pedido_t* pedido;
@@ -46,36 +52,40 @@ bool pedido_cambiar_cant (pedido_t* pedido, int nueva_cant){
 // lista_borrar_primero para sacar pedidos.
 
 
-// Crea una lista pedidos_entrantes
-lista_t* pedidos_entrantes_crear (){
-	lista_t* pedidos_entrantes;
-	pedidos_entrantes = lista_crear();
-	lista_iter_t* iter = lista_iter_crear(const lista_t* pedidos_entrantes);
+// Crea una objeto pedidos_entrantes
+pedidos_entrantes_t* pedidos_entrantes_crear(){
+	pedidos_entrantes_t* pedidos_entrantes;
+	pedidos_entrantes = malloc (sizeof(pedidos_entrantes_t));
+	if (!pedidos_entrantes) return NULL;
+	lista_t* lista_pedidos;
+	lista_pedidos = lista_crear();
+	lista_iter_t* iter = lista_iter_crear(lista_pedidos);
 	return pedidos_entrantes;
 	}
+	
 // devuelve la cantidad de pedidos que contiene la lista pedidos_entrantes
-size_t pedidos_entrantes_largo(const lista_t *pedidos_entrantes){
-	return pedidos_entrantes->largo;
+size_t pedidos_entrantes_largo(const pedidos_entrantes_t* pedidos_entrantes){
+	return pedidos_entrantes->lista_pedidos->largo;
 	}
 	
 // Agrega pedido al final de la lista.
-bool pedidos_entrantes_agregar (lista_t* pedidos_entrantes, pedido_t* pedido){
-	if (lista_insertar_ultimo(pedidos_entrantes, pedido)) return true;
+bool pedidos_entrantes_agregar (pedidos_entrantes_t* pedidos_entrantes, pedido_t* pedido){
+	if (lista_insertar_ultimo(pedidos_entrantes->lista_pedidos, pedido)) return true;
 	return false;
 	}
 
 // Elimina el primer pedido de la lista.
-pedido_t* pedidos_entrantes_sacar (lista_t* pedidos_entrantes){
-	return (lista_borrar_primero(pedidos_entrantes));
+pedido_t* pedidos_entrantes_sacar (pedidos_entrantes_t* pedidos_entrantes){
+	return (lista_borrar_primero(pedidos_entrantes->lista_pedidos));
 	}
 
 // Modifica la zona de un pedido.
 // Recibe: lista de pedidos_entrantes, iterador de esa lista, un int con la nueva zona
 // y un unsigned int id que identifica al pedido a modificar.
-bool pedidos_entrantes_zona (lista_t* pedidos_entrantes, lista_iter_t* iter, unsigned int id, int nueva_zona){
-	if (pedidos_entrantes->largo == 0) return false;
-	lista_iter_t* iter_pedido_a_modif = lista_iter_buscar(iter, id);
-	pedido_t* pedido_a_modif = lista_iter_ver_actual(pedidos_entrantes, iter_pedido_a_modif);
+bool pedidos_entrantes_zona (pedidos_entrantes_t* pedidos_entrantes, unsigned int id, int nueva_zona){
+	if (pedidos_entrantes->lista_pedidos->largo == 0) return false;
+	lista_iter_t* iter_pedido_a_modif = lista_iter_buscar(pedidos_entrantes->iter_lista_pedidos, id);
+	pedido_t* pedido_a_modif = lista_iter_ver_actual(pedidos_entrantes->lista_pedidos, iter_pedido_a_modif);
 	pedido_cambiar_zona(pedido_a_modif, nueva_zona);
 	return true;
 	}
@@ -83,18 +93,18 @@ bool pedidos_entrantes_zona (lista_t* pedidos_entrantes, lista_iter_t* iter, uns
 // Modifica cant_pizzas de un pedido
 // Recibe: lista de pedidos_entrantes, iterador de esa lista, un int con la nueva cantidad
 // y un unsigned int id que identifica al pedido a modificar.
-bool pedidos_entrantes_cant_pizzas (lista_t* pedidos_entrantes, lista_iter_t* iter, unsigned int id, int nueva_cant){
-	if (pedidos_entrantes->largo == 0) return false;
-	lista_iter_t* iter_pedido_a_modif = lista_iter_buscar(iter, id);
-	pedido_t* pedido_a_modif = lista_iter_ver_actual(pedidos_entrantes, iter_pedido_a_modif);
+bool pedidos_entrantes_cant_pizzas (pedidos_entrantes_t* pedidos_entrantes, unsigned int id, int nueva_cant){
+	if (pedidos_entrantes->lista_pedidos->largo == 0) return false;
+	lista_iter_t* iter_pedido_a_modif = lista_iter_buscar(pedidos_entrantes->iter_lista_pedidos, id);
+	pedido_t* pedido_a_modif = lista_iter_ver_actual(pedidos_entrantes->lista_pedidos, iter_pedido_a_modif);
 	pedido_cambiar_zona(pedido_a_modif, nueva_cant);
 	return true;
 	}
 
-bool cancelar_pedido (lista_t* pedidos_entrantes, lista_iter_t* iter, unsigned int id){
-	if (pedidos_entrantes->largo == 0) return false;
-	lista_iter_t* iter_pedido_a_cancelar = lista_iter_buscar(iter, id);
-	pedido_t* pedido_a_cancelar = lista_borrar(pedidos_entrantes, iter_pedido_a_cancelar);
+bool cancelar_pedido (pedidos_entrantes_t* pedidos_entrantes, unsigned int id){
+	if (pedidos_entrantes->lista_pedidos->largo == 0) return false;
+	lista_iter_t* iter_pedido_a_cancelar = lista_iter_buscar(pedidos_entrantes->iter_lista_pedidos, id);
+	pedido_t* pedido_a_cancelar = lista_borrar(pedidos_entrantes->lista_pedidos, iter_pedido_a_cancelar);
 	pedido_destruir(pedido_a_cancelar);
 	return true;
 	}
