67574db6a719d8c03b9324cb0904bb56420d0c7d
Facundo Acevedo <acv2facundo@gmail.com>
2012-09-29 17:18:20 -0300

    cambiado iterador por iter en buscador

--- buscador.c
+++ buscador.c
@@ -7,26 +7,26 @@
  *Busca sobre una lista, y devuelve un nodo.
  *PRE: recibe una pedidos_entrates_t con la lista de pedidos, y un unsigned int con el
  *id a buscar
- *POST: mueve el iterador hasta el nodo donde esta ese id, si no lo encuentra, 
+ *POST: mueve el iter hasta el nodo donde esta ese id, si no lo encuentra, 
  *devuelve false o true de caso contrario
  */
 lista_iter_t* buscar_id(pedidos_entrantes_t* pedidos, unisegned int id)
 {
     //variables
     lista_t* lista = pedidos->lista;
-    lista_iter_t* iterador = pedidos->iterador;
+    lista_iter_t* iter = pedidos->iter;
     nodo_t* nodo;
     unsigned int id_cmp;
 
 
-    //reinicio la posicion del iterador
+    //reinicio la posicion del iter
     iter->actual = lista.inicio;
     iter->siguiente = (iter->actual)->siguiente;
 
     //Busco
     while(true)
     {
-        nodo = iterador->actual;
+        nodo = iter->actual;
         id_cmp = (nodo->datos)->id;
         
         if (id_cmp == id)
@@ -34,7 +34,7 @@ lista_iter_t* buscar_id(pedidos_entrantes_t* pedidos, unisegned int id)
         else if (nodo->siguiente == NULL)
             break
 
-       iter_avanzar(iterador);     
+       iter_avanzar(iter);     
     }
     return false;
     
