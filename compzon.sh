6e1b9a75dc072691c095c9b0bc71ffba7821fa5c
Usuario <usuario@MacBook-Air-de-Usuario.local>
2012-10-03 19:35:47 -0300

    tdas

--- compzon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-OPCIONES="-std=c99 -Wall -pedantic -g"
-
-#~ gcc $OPCIONES -c listaycola.c lista_con_iter.c pedidos_entrantes.c pila.c
-gcc $OPCIONES -c listaycola.c lista_con_iter.c 
-gcc $OPCIONES listaycola.o lista_con_iter.o zonas.c -o zonas.bin
