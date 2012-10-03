bec482ca79c4cf0368b8cc0d610aac560fbcd0c1
FacundoAcevedo <acv2Facundo@gmail.com>
2012-10-03 19:39:09 -0300

    correccion en nombre

--- pedidos.c
+++ pedidos.c
@@ -1,10 +1,8 @@
-#include "pila.h"
-#include "pedidos.h"
-#include <stdbool.h>
-#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <stddef.h>
+#include "pedidos.h"
 /*##############################################################*/
 /*SALIENTES*/
 /*##############################################################*/
