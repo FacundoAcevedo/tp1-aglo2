1554411ce621a93f324715b649b69b555a2f9952
Usuario <usuario@MacBook-Air-de-Usuario.local>
2012-10-03 19:16:16 -0300

    union de zonasymotos

---
+++ zonasymoto.h
@@ -0,0 +1,29 @@
+#ifndef ZONASYMOTO_H
+#define ZONASYMOTO_H
+#include "pila.h"
+
+//########################################################################
+//                                PRIMITIVAS DE ZONAS
+//########################################################################
+typedef struct zona zona_t;
+
+zona_t* zona_crear();
+
+bool zona_preparar_pedidos(zona_t** vector, lista_con_iter_t* pedidos);
+
+pedido_t* zona_sacar (zona_t** vector, int zona_num);
+
+//########################################################################
+//                                PRIMTIVAS DE ZONAS
+//########################################################################
+
+pila_t* moto_cargar(zona_t* zona);
+
+pedido_t* buscar_adecuado (lista_con_iter_t* lista_espera, int pizzas_cargadas) ;
+
+int intentar_lista_espera(lista_con_iter_t* zona, pila_t* moto);
+
+int intentar_cola_ppal(lista_con_iter_t* zona, pila_t* moto);
+
+
+#endif
