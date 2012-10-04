595157dd79395a39d6405d3ed209e2e3d014b5a1
FacundoAcevedo <acv2Facundo@gmail.com>
2012-10-03 21:14:37 -0300

    Un rungcc que anda

--- rungcc.sh
+++ rungcc.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 OPCIONES="-std=c99 -Wall -pedantic -g"
-ARCHIVOS_O="lista.o  cola.o  pila.o  pedidos_entrantes.o  pedidos_salientes.o  buscador.o  moto.o  zonas.o"
-ARCHIVOS_C="lista.c  cola.c  pila.c  pedidos_entrantes.c  pedidos_salientes.c  buscador.c  moto.c  zonas.c"
 
 
 
-gcc $OPCIONES -c $ARCHIVOS_C
-gcc $OPCIONES $ARCHIVOS_O -o prurebas.bin
+O="zonasymoto.o vect_din.o  pedidos.o  tdas.o" 
+C="zonasymoto.c vect_din.c  pedidos.c  tdas.c" 
+
+
+gcc $OPCIONES -c $C;
+gcc $OPCIONES $O main.c -o main.bin;
