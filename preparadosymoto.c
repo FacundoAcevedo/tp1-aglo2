f183057ba6303dfb16f96aa3fe42ae6748e07938
Usuario <usuario@MacBook-Air-de-Usuario.local>
2012-10-08 01:12:13 -0300

    agregados los archivos renombrados

---
+++ preparadosymoto.c
@@ -0,0 +1,141 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "preparadosymoto.h"
+#include "tdas.h"
+#include "pedidos.h"
+#include "pantalla.h"
+
+
+void preparados_destruir(lista_t* preparados, void destruir_pedido(void*)){
+	lista_destruir(preparados, (*destruir_pedido));
+	return;
+	}
+
+
+bool zona_preparar_pedidos(lista_t* preparados, lista_t* pedidos_entrantes){
+	// Si pedidos_entrantes no existe
+	if (!pedidos_entrantes) return false;
+	// Si la lista de pedidos_entrantes esta vacia
+	if (lista_esta_vacia(pedidos_entrantes)) return false;
+	// Reparto entre las preparados
+	while (lista_largo(pedidos_entrantes) > 0){
+		pedido_t* pedido;
+		pedido = pedidos_entrantes_sacar(pedidos_entrantes);
+		lista_insertar_ultimo(preparados, pedido);
+		}
+	return true;
+	}
+
+
+lista_t* moto_cargar(lista_t* preparados, pila_t* salientes){
+	// Si no hay pedidos para las preparados devuelvo NULL.
+	if (lista_esta_vacia(preparados)) return false;
+
+	// Creo la moto
+	lista_t* moto = lista_crear();
+	size_t cant_en_moto;
+	cant_en_moto = lista_largo(moto);
+	// Creo un iterador de preparados.
+	lista_iter_t* iter;
+	iter = lista_iter_crear(preparados);
+	// Guardo su largo para usarlo como condicion de corte
+	int num_iter;
+	num_iter = 0;
+	
+	pedido_t* pedido;
+	pedido = lista_iter_ver_actual(iter);
+	
+	// Determino la zona para la cual enviare la moto
+	int zona;
+	zona = pedido->zona;
+	
+	// Agrego este pedido en la moto y lo saco de la lista preparados
+	lista_insertar_primero(moto, pedido);
+	pila_apilar(salientes, pedido);
+
+	lista_borrar(preparados, iter);
+		
+	cant_en_moto += pedido->cant_pizzas;
+
+	while (cant_en_moto < 5){
+		
+		pedido_t* actual = lista_iter_ver_actual(iter);
+		int suma;
+		suma = cant_en_moto + actual->cant_pizzas;
+		if ((suma <= 5) && (actual->zona == zona)){
+			cant_en_moto += actual->cant_pizzas;
+			lista_insertar_primero(moto, actual);
+			lista_borrar(preparados, iter);
+			// Apilo este pedido a pedidos_salientes
+			pila_apilar(salientes, actual);
+
+			// Si llegue a la capacidad maxima, ordeno la moto y la retorno 
+			if ((cant_en_moto) == 5){
+				lista_t* moto_ordenada= moto_ordenar(moto);
+				return moto_ordenada;
+			}
+		}
+		
+		// el pedido actual no cumplio los requisitos de zona y/o pedido,
+		// paso al siguiente. Condicion: no estar al final de la lista.
+		// Si llegue al final, corto el while.
+		num_iter += 1;
+		if (num_iter <= (int)lista_largo(preparados)){
+			lista_iter_avanzar(iter);
+		}
+		if (num_iter > (int)lista_largo(preparados)){
+			break;
+		}
+	}
+	lista_t* moto_ordenada = moto_ordenar(moto);
+	return moto_ordenada;
+}
+
+
+lista_t* moto_ordenar(lista_t* moto){
+	lista_t* orden = lista_crear();
+	while (!lista_esta_vacia(moto)){
+		pedido_t* borrado = lista_borrar_primero(moto);
+		// Si la lista ordenada esta vacia, inserto
+		if (lista_esta_vacia(orden)) lista_insertar_primero(orden, borrado);
+		// SI la lista ordenada tiene 1 elemento
+		else if (lista_largo(orden) == 1){
+			lista_iter_t* iter = lista_iter_crear(orden);
+			pedido_t* pedido = lista_iter_ver_actual(iter);
+			int dist = pedido->distancia;
+			// Si el que tengo que insertar es mayor al elemento de la lista ordenada
+			// lo inserto en el 2do lugar con insertar_ultimo
+			if (borrado->distancia >= dist) 
+				lista_insertar_ultimo(orden, borrado);
+			// Si es menor, lo inserto en el primer lugar
+			else if (borrado->distancia < dist) 
+				lista_insertar_primero(orden, borrado);
+			lista_iter_destruir(iter);
+		}
+			// Si la lista ordenada tiene mas de un elemento:
+		else{
+			// Recorro la lista buscando un valor mayor 
+			int i = 0;
+			while(i<lista_largo(moto)){
+				lista_iter_t* iter_orden = lista_iter_crear(orden);
+				pedido_t* actual = lista_iter_ver_actual(iter_orden);
+				// si encuentro un valor mayor, inserto antes de el
+				// y corto este while
+				if (actual->distancia > borrado->distancia){
+					lista_insertar(orden, iter_orden, borrado);
+					lista_iter_destruir(iter_orden);
+					break;
+				}
+				// Si llegue al final y no encontre mayor, inserto en ultimo lugar
+				lista_insertar_ultimo(orden, borrado);
+				lista_iter_avanzar(iter_orden);
+				i +=1;
+			}
+		}
+	}
+	lista_destruir(moto, destruir_pedido);
+	return orden;
+}
+	
---
+++ preparadosymoto.h
@@ -0,0 +1,30 @@
+#ifndef PREPARADOSYMOTO_H
+#define PREPARADOSYMOTO_H
+#include "tdas.h"
+#include "pedidos.h"
+#include "stdbool.h"
+
+//########################################################################
+//                        PRIMITIVAS DE PREPARADOS Y MOTO
+//########################################################################
+
+// Destruye la lista "preparados" y los pedidos que contiene
+void preparados_destruir(lista_t* preparados, void destruir_pedido(void*));
+
+// Pasa los pedidos de pedidos_entrantes a la lista preparados
+bool zona_preparar_pedidos(lista_t* preparados, lista_t* pedidos_entrantes);
+
+// Recibe la lista preparados y saca de ella los pedidos que cumplan los requisitos:
+// la cantidad de pizzas en la moto debe ser mayor o igual a la capacidad
+// maxima. Recibe también la pila "salientes" a donde ira apilando los pedidos
+// cargados.
+// Devuelve una lista moto ordenada si la moto se cargo con pizzas; NULL
+// si pedidos_entrantes estaba vacia o  la moto no pudo cargar nada 
+lista_t* moto_cargar(lista_t* preparados, pila_t* salientes);
+
+// Recibe una lista "moto" y la devuelve ordenada por la distancia
+// de los pedidos que contiene de menor a mayor
+lista_t* moto_ordenar(lista_t* moto);
+
+
+#endif
