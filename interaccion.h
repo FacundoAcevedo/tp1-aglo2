826c4cf554bd4cc291265545ca6a70f4b82ecb20
Usuario <usuario@MacBook-Air-de-Usuario.local>
2012-11-10 18:21:51 -0300

    Arreglado el menu

---
+++ interaccion.h
@@ -0,0 +1,15 @@
+#ifndef INTERACCION_H
+#define INTERACCION_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+char* leer_texto();
+
+//Funcion de Potencia recursiva, para que sea mÃ¡s eficiente.
+int pot(int x, int y);
+
+int leer_numero();
+
+#endif
